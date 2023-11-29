fcee3d034583b09a9417d11c19383ac7693e3800
toluOjo <ojotolulope137@gmail.com>
2023-11-29 12:56:06 +0100

    implement circuit breaker via resilient4j

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("api/order")
@@ -13,8 +18,15 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public String placeOrder(@RequestBody OrderRequest orderRequest ){
-         orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest ){
+       return  CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest)) ;
+
+    }
+
+    public CompletableFuture<String> fallBackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()->"Oops Something Went Wrong!!.");
     }
 }
--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
     private final WebClient.Builder webClientBuilder;
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
       List<OrderLineItems> orderLineItems= orderRequest.getOrderLineItemsDtoList()
@@ -41,6 +41,7 @@ public class OrderService {
         boolean allProductsIsInStock= Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
        if(allProductsIsInStock){
            orderRepository.save(order);
+           return "Order Placed Successfully!!";
        }else{
            throw new IllegalArgumentException("Product is not in stock");
        }
