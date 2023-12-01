3667c192c945aef4c08489efe68deeb9fcdb9e74
toluOjo <ojotolulope137@gmail.com>
2023-12-01 10:05:23 +0100

    implement notification service and apache kafka messaging

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.example.orderservice.service;
 import com.example.orderservice.dto.InventoryResponse;
 import com.example.orderservice.dto.OrderLineItemsDto;
 import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.event.OrderPlacedEvent;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.OrderLineItems;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,6 +22,8 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final WebClient.Builder webClientBuilder;
+
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
     private final OrderRepository orderRepository;
     public String placeOrder(OrderRequest orderRequest){
@@ -41,6 +45,7 @@ public class OrderService {
         boolean allProductsIsInStock= Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
        if(allProductsIsInStock){
            orderRepository.save(order);
+           kafkaTemplate.send("notificationTopic",new OrderPlacedEvent(order.getOrderNumber()));
            return "Order Placed Successfully!!";
        }else{
            throw new IllegalArgumentException("Product is not in stock");
